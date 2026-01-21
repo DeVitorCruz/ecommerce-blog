492df1da79ef2e63a625d524d21b26a334ac2cc5
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2026-01-21 16:56:47 -0300

    That was added the items to the logo card slide.

---
+++ src/app/home/components/main/components/partner/constants/partner-list/PARTNER_LIST.ts
@@ -0,0 +1,28 @@
+import { Card } from "../../../../../../../shared/interfaces/card/Card";
+
+export const PARTNER_LIST: Card[] = [
+    {
+        path: 'assets/partner/p-logo1.png',
+        alt: 'Partner-1',
+    },
+    {
+        path: 'assets/partner/p-logo2.png',
+        alt: 'Partner-2',
+    },
+    {
+        path: 'assets/partner/p-logo3.png',
+        alt: 'Partner-3',
+    },
+    {
+        path: 'assets/partner/p-logo4.png',
+        alt: 'Partner-4',
+    },
+    {
+        path: 'assets/partner/p-logo5.png',
+        alt: 'Partner-5',
+    },
+    {
+        path: 'assets/partner/p-logo6.png',
+        alt: 'Partner-6',
+    }
+];
